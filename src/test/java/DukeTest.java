0c49d53476a18efea1e5665812835c25db524233
tzw0 <e0310410@u.nus.edu>
2019-09-08 00:00:04 +0800

    messed up

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,9 +1,42 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DukeTest {
     @Test
     public void dummyTest(){
         assertEquals(2, 2);
+    }
+    @Test
+    public void DeadlineTest() {
+        new DeadlineTest().Test();
+    }
+    @Test
+    public void DeleteTest() {
+        new DeadlineTest().Test();
+    }
+    @Test
+    public void DoneTest() {
+        new DeadlineTest().Test();
+    }
+    @Test
+    public void EventTest() {
+        new DeadlineTest().Test();
+    }
+    @Test
+    public void FindTest() {
+        new DeadlineTest().Test();
+    }
+    @Test
+    public void ListTest() {
+        new DeadlineTest().Test();
+    }
+    @Test
+    public void ParserTest() {
+        new DeadlineTest().Test();
+    }
+    @Test
+    public void Todo() {
+        new DeadlineTest().Test();
     }
 }
--- src/test/java/ListTest.java
+++ src/test/java/ListTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TodoTest {
+public class ListTest {
     @Test
     public void dummyTest(){
         assertEquals(2, 2);
